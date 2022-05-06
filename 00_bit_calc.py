fb368d7ff8ec35584a32f45429b6b05c9e0e1cf3
simeonk1234 <101219115+simeonk1234@users.noreply.github.com>
2022-05-06 14:49:32 +1200

    Add files via upload

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -15,5 +15,52 @@ def statement_generator(text, decoration):
 
     return""
 
+
+def user_choice():
+
+    #Lists of valid responses
+    text_ok = ["text", "t", "txt"]
+    integer_ok = ["integer", "int", "#", "number"]
+    image_ok = ["image", "img", "pix", "picture", "pic", "p"]
+
+    valid = False
+    while not valid:
+        # ask user for choice and change response to lowercase
+        response = input("File type (integer / text / image): ").lower()
+
+        # Checks for valid response and returns text, integer or image
+
+        
+        if response in text_ok:
+            return "text"
+        
+        elif response in integer_ok:
+            return "integer"
+        
+        elif response in image_ok:
+            return "image"
+
+        elif response == "i":
+            want_integer = input("Press <enter> for an integer or any key for an image: ")
+            if want_integer == "":
+                return "integer"
+            else:
+                return "image"
+ 
+        else:
+            # if response is not valid, output an error
+            print("Please choose a valid file type!")
+            print()
+
 # Main routine goes here
-statement_generator("look - stars", "*")
+
+# Heading
+statement_generator ("Bit Calculator for Integers, Texts & Images", "-")
+
+# Display instructions if user has not used the programme before
+
+# Loop to allow multiple calculations per session
+keep_going = ""
+while keep_going == "":
+
+    # Ask the user for the file type
--- 02_input_checker.py
+++ 02_input_checker.py
@@ -1,25 +1,46 @@
 # checks user choice is 'integer', 'text', or 'image'
 def user_choice():
 
+    #Lists of valid responses
+    text_ok = ["text", "t", "txt"]
+    integer_ok = ["integer", "int", "#", "number"]
+    image_ok = ["image", "img", "pix", "picture", "pic", "p"]
+
     valid = False
     while not valid:
-
         # ask user for choice and change response to lowercase
         response = input("File type (integer / text / image): ").lower()
 
+        # Checks for valid response and returns text, integer or image
 
-        #If they choose "t" or "text", return "text"   
-        if response == "t":
-            return response
+        
+        if response in text_ok:
+            return "text"
+        
+        elif response in integer_ok:
+            return "integer"
+        
+        elif response in image_ok:
+            return "image"
 
+        elif response == "i":
+            want_integer = input("Press <enter> for an integer or any key for an image: ")
+            if want_integer == "":
+                return "integer"
+            else:
+                return "image"
+ 
         else:
-            # if response is not valid, output an error 
+            # if response is not valid, output an error
             print("Please choose a valid file type!")
             print()
 
+
 # Main routine goes here
-data_type = user_choice()
-print("You chose", data_type)
+keep_going = ""
+while keep_going == "": 
+    data_type = user_choice()
+    print("You chose", data_type)
 
-print()
 
+    print()
